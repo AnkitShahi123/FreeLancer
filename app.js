8d2daed4441f099782e02cc39fd99342f222bcad
aavashneupane <aavashneupane4@gmail.com>
2021-07-05 20:43:10 +0545

    configured app.js file for server

---
+++ app.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-Parser');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cors());
+
+
+app.listen(89, () => {
+  console.log("Server running at 89.");
+});
