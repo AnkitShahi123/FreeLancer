5e2349c2250ba3e73ba11cf6b19545f5d6de3531
aavashneupane <aavashneupane4@gmail.com>
2021-07-07 17:03:41 +0545

    fixed issues while uploading image in form

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const bodyParser = require('body-Parser');
-const cors = require('cors');
-
-const app = express();
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/images',express.static(__dirname+'/images'));
-
-app.use(cors());
-
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
 const db = require("./database/db");
 const user = require("./routes/user");
+// const path = require('path');
+const app = express();
 
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use("/images", express.static(__dirname + "/images"));
+app.use(cors());
 app.use(user);
 
 app.listen(89, () => {
--- routes/user.js
+++ routes/user.js
@@ -12,21 +12,7 @@ const auth = new authController();
 const token = tokenVerification.verifyUser;
 
 //adding register api
-router.post(
-  "/user/add",
-  [
-    check("firstname", "First Name must be entered").not().isEmpty(),
-    check("lastname", "Last Name must be entered").not().isEmpty(),
-    check("email", "Invalid Email").isEmail(),
-    check("password", "You must enter your password").not().isEmpty(),
-    check("password", "it must be 8 to 16 length long").isLength({
-      min: 8,
-      max: 16,
-    }),
-  ],
-  upload.single('photo'),
-  auth.register
-);
+router.post("/user/add", upload.single("photo"), auth.register);
 
 router.post("/user/login", auth.login);
 
