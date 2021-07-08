06f4c4bd86633bc3f9b1bb9e96c3351848c99d6a
aavashneupane <aavashneupane4@gmail.com>
2021-07-08 21:37:57 +0545

    added education in models and api

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const auth = require("../middleware/auth");
 const upload = require("../middleware/uploads");
+const uploadPdf = require("../middleware/uploadPdf");
 const profileController = require("../controllers/profileController");
 const profile = new profileController();
 
@@ -31,8 +32,10 @@ router.put(
 router.put(
   "/profile/editResume/:id",
   [check("resume", "You must upload a file before saving.").not().isEmpty()],
-  upload.single("resume"),
+  uploadPdf.single("resume"),
   profile.editResume
 );
+
+
 
 module.exports = router;
