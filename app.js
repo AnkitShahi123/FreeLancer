e5c4336c262f1dbb88fa715b85331d6fa4d7c7a0
aavashneupane <aavashneupane4@gmail.com>
2021-07-11 12:14:06 +0545

    added port

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-const express = require('express');
-const bodyParser = require('body-Parser');
-const cors = require('cors');
-
-const app = express();
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use('/images',express.static(__dirname+'/images'));
-
-app.use(cors());
-
-const db = require("./database/db");
-const user = require("./routes/user");
-
-app.use(user);
-
-app.listen(89, () => {
-=======
 const mongoose = require("mongoose");
 const express = require("express");
 
@@ -41,7 +20,6 @@ app.use(cors());
 app.use(user);
 app.use(profileRoute);
 
-app.listen(process.env.PORT, () => {
->>>>>>> 8baf73ee0d84e45cedbc36c8314e135fddc95f84
+app.listen(process.env.PORT ||89, () => {
   console.log("Server running at 89.");
 });
