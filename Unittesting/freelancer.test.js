fdaf524b6699b3d3280d29d005bf88f1eb763641
Kuzz07 <kushalphaiju@gmail.com>
2021-07-15 17:14:31 +0545

    Client registration testing

--- Unittesting/freelancer.test.js
+++ Unittesting/freelancer.test.js
@@ -29,31 +29,60 @@ afterAll(async () => {
 
 
 
-// //USER ADMIN REGISTRATION TESTING
+// USER ADMIN REGISTRATION TESTING
+
+// describe('User Schema test anything', () => { // the code below is for insert testing
+//     it('Add User testing anything', () => {
+//         const register = {
+
+//             'firstname': 'sashank',
+//             'lastname': 'shrestha',
+//             'email': 'sashank@gmail.com',
+//             "password": "password1",
+//             "address": "Thamel",
+//             "phone": "9869688543",
+//             "age": 21,
+//             "role": "Admin",
+//             "projects": "Jobportal",
+//             "Exprience": "1 year",
+//             "company": "Softwarica",
+//             "foundedin": "Nepal",
+//             "userbio": "my name is nibbu",
+//             "resume": "ihave",
+
+//         };
+
+//         return registration.create(register).then((pro_ret) => {
+//             expect(pro_ret.firstname).toEqual('sashank');
+//         });
+//     });
+
+// });
+
+//USER CLIENT REGISTRATION TESTING
 
 describe('User Schema test anything', () => { // the code below is for insert testing
     it('Add User testing anything', () => {
         const register = {
 
-            'firstname': 'sashank',
-            'lastname': 'shrestha',
-            'email': 'sashank@gmail.com',
-            "password": "password1",
-            "address": "Thamel",
-            "phone": "9869688543",
-            "age": 21,
-            "role": "Admin",
+            'firstname': 'Ankit',
+            'lastname': 'lama',
+            'email': 'lama12@gmail.com',
+            "password": "password2",
+            "address": "boudha",
+            "phone": "9869688544",
+            "age": 22,
+            "role": "Client",
             "projects": "Jobportal",
-            "Exprience": "1 year",
-            "company": "Softwarica",
+            "Exprience": "2 year",
+            "company": "Softwaricaedu",
             "foundedin": "Nepal",
-            "userbio": "my name is nibbu",
-            "resume": "ihave",
-
+            "userbio": "my name is ankit",
+            "resume": "ihave",           
         };
 
         return registration.create(register).then((pro_ret) => {
-            expect(pro_ret.firstname).toEqual('sashank');
+            expect(pro_ret.firstname).toEqual('Ankit');
         });
     });
 
