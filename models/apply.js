da8127853ea29e1822ddebb2c7fd5f682e22c2e2
aavashneupane <aavashneupane4@gmail.com>
2021-07-30 19:17:51 +0545

    added apply model

---
+++ models/apply.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+var User = require("./user");
+var Work = require("./work");
+var SCHEMA = mongoose.Schema;
+
+const applySchema = new SCHEMA({
+  userid: {
+    type: SCHEMA.Types.ObjectId,
+    ref: User,
+  },
+  workid: {
+    type: SCHEMA.Types.ObjectId,
+    ref: Work,
+  },
+  myamount:{
+    type: String,
+    required: true
+  },
+  confirmStatus: {
+    type: String,
+    default: "pending",
+  },
+  video: {
+    type: String,
+    
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const APPLY = mongoose.model("applied", applySchema);
+module.exports = APPLY;
